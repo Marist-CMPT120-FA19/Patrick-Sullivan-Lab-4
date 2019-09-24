9d6838c8da2e247bf77eb97664d957631c565d80
Patrick Sullivan <16sullivanp@gmail.com>
2019-09-23 22:03:05 -0400

    Initial commit

---
+++ trafficlight.py
@@ -0,0 +1,25 @@
+>>> from graphics import*
+>>> win=GraphWin()
+>>> def main():
+	win= GraphWin()
+	shape= Rectangle(Point(2,20), Point(40,100))
+	shape.setOutline("black")
+	shape.setFill("white")
+	shape.draw(win)
+	red=Circle(Point(20,40),8)
+	red.setOutline("black")
+	red.setFill("red")
+	red.draw(win)
+	yellow=Circle(Point(20,60), 8)
+	yellow.setOutline("black")
+	yellow.setFill("yellow")
+	yellow.draw(win)
+	green=Circle(Point(20,80),8)
+	green.setOutline("black")
+	green.setFill("green")
+	green.draw(win)
+	win.getMouse()
+	win.close()
+
+	
+>>> main()
